29c3267daea806ffdfc0e07aec3ab1703c934571
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-04-28 11:26:35 -0400

    Update analysis.py

--- Analysis/iris/analysis.py
+++ Analysis/iris/analysis.py
@@ -1,10 +1,12 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 with open('/Raw-Data-Files/iris/iris.data') as raw:
     data = raw.read().splitlines()
 
 data[0:5]
 
+# Clean Data
 sData = [obs.split(',') for obs in data]
 
 sData[0:5]
@@ -17,3 +19,17 @@ for col in numCols:
     df[col] = df[col].astype(float)
 
 df.head()
+
+# Visualize Data
+for col in numCols:
+    plt.hist(df[col])
+    plt.title(col)
+    plt.show()
+    
+# Split Data
+dfShuff = df.sample(frac=1)
+
+prop = .8
+l = round(prop*len(df))
+train = dfShuff[0:l]
+test = dfShuff[l:]
