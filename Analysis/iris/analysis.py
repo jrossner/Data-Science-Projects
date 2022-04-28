de75705a8bc4df35ea77264b2b51efa1561bd78c
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-04-28 11:50:27 -0400

    Update analysis.py
    Ann imp

--- Analysis/iris/analysis.py
+++ Analysis/iris/analysis.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
 
 with open('/Raw-Data-Files/iris/iris.data') as raw:
     data = raw.read().splitlines()
@@ -28,8 +32,41 @@ for col in numCols:
     
 # Split Data
 dfShuff = df.sample(frac=1)
-
 prop = .8
 l = round(prop*len(df))
+
 train = dfShuff[0:l]
 test = dfShuff[l:]
+
+# KNN
+
+trainX = train[numCols]
+trainY = train['iris']
+testX = test[numCols]
+testY = test['iris']
+
+scaler = StandardScaler()
+scaler.fit(trainX)
+
+trainXS = scaler.transform(trainX)
+testXS = scaler.transform(testX)
+
+k = round(len(trainY)**.5)
+model = KNeighborsClassifier(n_neighbors=k)
+model.fit(trainXS,trainY)
+
+preds = model.predict(testXS)
+
+print(f'MSE: ${np.mean(preds != testY)}')
+confusion_matrix(testY,preds)
+
+mErr = []
+for kn in range(1,k*2):
+    knn = KNeighborsClassifier(kn)
+    knn.fit(trainXS,trainY)
+    pred_i = knn.predict(testXS)
+    mErr.append(np.mean(pred_i != testY))
+
+    
+plt.xticks(range(1,k*2))
+plt.plot(range(1,k*2),mErr,marker='o')
