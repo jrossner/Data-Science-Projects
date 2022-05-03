335feffb1011ea77efd2cefaa9da7678dc8f1e2e
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-05-03 13:27:29 -0400

    Update lcid-price-analysis.py

--- Analysis/stocks/lcid-price-analysis.py
+++ Analysis/stocks/lcid-price-analysis.py
@@ -45,10 +45,52 @@ for p in range(0,15):
 
 #find model with best AIC
 df = pd.DataFrame(models)
-lowest_AIC_model = df[df["AIC"] == df["AIC"].max()]
+lowest_AIC_model = df[df["AIC"] == df["AIC"].min()]
 best_p,best_d,best_q = int(lowest_AIC_model['p']), int(lowest_AIC_model['d']),int(lowest_AIC_model['q'])
 
 #once best AIC has been found
 best_model = ARIMA(timeSeries, order = (best_p, best_d, best_q))
 best_fit = best_model.fit()
 best_fit.summary()
+
+#check residuals of model
+plt.hist(best_fit.resid)
+plt.plot(best_fit.resid)
+plt.show()
+
+#check fit
+fit_vals = best_fit.fittedvalues
+plt.plot(timeSeries,color='blue')
+plt.plot(fit_vals,color='orange')
+plt.show()
+
+#check prediction
+length = len(timeSeries)
+tl = round(length*.8)
+train = timeSeries[:tl]
+test = timeSeries[tl:]
+
+model = ARIMA(train, order = (best_p, best_d, best_q))
+fit = model.fit()
+preds = fit.forecast(steps=2*len(test))
+
+trainD = [x for x in train]
+
+testD = []
+for x in train:
+    testD.append(None)
+
+for y in test:
+    testD.append(y)
+
+predD = []
+for y in train:
+    predD.append(None)
+
+for y in preds:
+    predD.append(y)
+
+plt.plot(trainD,color='blue')
+plt.plot(testD,color='orange')
+plt.plot(predD,color='red',linestyle='-')
+plt.show()
