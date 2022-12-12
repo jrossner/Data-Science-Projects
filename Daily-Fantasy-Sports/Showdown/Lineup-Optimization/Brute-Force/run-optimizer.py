53f7c8e958a093cf08ed844f5ac1b28f6506892d
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-12-12 18:34:12 -0500

    Update run-optimizer.py

--- Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/run-optimizer.py
+++ Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/run-optimizer.py
@@ -8,10 +8,10 @@ raw = pd.DataFrame({"player": d["Name"], "salary": d["Salary"], "points": d["Avg
 out = ["Rondale Moore","Jakobi Meyers","Brian Hoyer", "Quinn Nordin","Bailey Zappe"]
 raw = raw[~raw["player"].isin(out)].reset_index(drop=True)
 
-players = list(raw_clean["player"].unique())
+players = list(raw["player"].unique())
 dset = pd.DataFrame({"player": [], "points": [], "salary": [], "capt. salary": [], "capt. points": []})
 for player in players:
-    d = raw_clean[raw_clean["player"] == player].sort_values(by = ["salary"],ascending = False).reset_index(drop=True)
+    d = raw[raw["player"] == player].sort_values(by = ["salary"],ascending = False).reset_index(drop=True)
     dset.loc[len(dset)] = [player, d["points"][1], d["salary"][1], d["salary"][0], d["points"][0]*1.5]
 
 dset = dset[dset["salary"] > 2000]
