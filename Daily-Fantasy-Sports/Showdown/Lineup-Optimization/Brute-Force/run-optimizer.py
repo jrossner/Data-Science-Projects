8693a1c50075e56b1cbe17e1bd3996046d74b490
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-12-12 16:02:04 -0500

    Update run-optimizer.py

--- Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/run-optimizer.py
+++ Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/run-optimizer.py
@@ -6,12 +6,12 @@ print(f'directories: {listdir()}')
 
 d = pd.read_csv("Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/data/DKSalaries.csv")
 
-arine = pd.DataFrame({"player": d["Name"], "salary": d["Salary"], "points": d["AvgPointsPerGame"]})
+arine = pd.DataFrame({"player": d["Name"], "capt. salary": d["Salary"], "points": d["AvgPointsPerGame"]})
 arine["capt. points"] = [ p * 1.5 for p in arine["points"]]
-arine["capt. salary"] = [ s * 1.5 for s in arine["salary"]]
-arine_2000 = arine[arine["salary"] > 1900]
+arine["salary"] = [ s / 1.5 for s in arine["capt. salary"]]
 
 out = ["Rondale Moore","Jakobi Meyers","Brian Hoyer", "Quinn Nordin"]
 arine = arine[~arine["player"].isin(out)].reset_index(drop=True)
+arine_clean = arine[arine["salary"] > 2000]
 
-print(f'Chosen Lineup: {pickLineup(arine_2000,budget=50000,num_utility=5)}')
+print(f'Chosen Lineup: {pickLineup(arine_clean,budget=50000,num_utility=5)}')
