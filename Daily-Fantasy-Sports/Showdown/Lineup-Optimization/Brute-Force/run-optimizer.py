1ed2b2a375fcffb8c82b7dd8bd139aac3ce4ca26
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-12-12 18:26:28 -0500

    Update run-optimizer.py

--- Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/run-optimizer.py
+++ Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/run-optimizer.py
@@ -2,16 +2,14 @@ from pickLineup import pickLineup
 import pandas as pd
 from os import listdir
 
-print(f'directories: {listdir()}')
-
 d = pd.read_csv("Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/data/DKSalaries.csv")
 
-arine = pd.DataFrame({"player": d["Name"], "salary": d["Salary"], "points": d["AvgPointsPerGame"]})
+raw = pd.DataFrame({"player": d["Name"], "salary": d["Salary"], "points": d["AvgPointsPerGame"]})
 out = ["Rondale Moore","Jakobi Meyers","Brian Hoyer", "Quinn Nordin","Bailey Zappe"]
-arine = arine[~arine["player"].isin(out)].reset_index(drop=True)
-arine_clean = arine[arine["salary"] > 2000]
+raw = raw[~raw["player"].isin(out)].reset_index(drop=True)
+raw_clean = raw[raw["salary"] > 2000]
 
-players = list(arine["player"].unique())
+players = list(raw_clean["player"].unique())
 dset = pd.DataFrame({"player": [], "points": [], "salary": [], "capt. salary": [], "capt. points": []})
 for player in players:
     d = arine[arine["player"] == player].sort_values(by = ["salary"],ascending = False).reset_index(drop=True)
