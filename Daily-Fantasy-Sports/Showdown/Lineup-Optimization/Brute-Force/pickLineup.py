3b8826cf7cf87f7cc527bf32411430c9c66379d2
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-12-12 13:09:00 -0500

    Create pickLineup.py

---
+++ Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/pickLineup.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from itertools import combinations as comb
+
+def pickLineup(data, budget = 50000, num_utility = 5):
+    best_lineup = {"players": [], "total": 0, "salary": 0, "captain": None}
+
+    for i in range(len(data)):
+        capt = data["player"][i]
+        c_salary = data["capt. salary"][i]
+        c_points = data["capt. points"][i]
+        remaining = budget - data["capt. salary"][i]
+
+        rems = data.drop([i]).reset_index()
+
+        utility_lineups = list(comb(rems["player"], num_utility))
+        for lineup in utility_lineups:
+            lineup = list(lineup)
+            lineup_data = rems[rems["player"].isin(lineup)]
+            if c_salary + sum(lineup_data["salary"]) <= remaining:
+                if c_points + sum(lineup_data["points"]) > best_lineup["total"]:
+                    lineup_total = c_points + sum(lineup_data["points"])
+                    lineup_salary = c_salary + sum(lineup_data["salary"])
+                    print("found a better line up")
+                    best_lineup = {"players": [capt]+list(lineup_data["player"]),
+                                    "total": lineup_total, "salary": lineup_salary,
+                                    "captain": capt}
+            
+    return best_lineup
