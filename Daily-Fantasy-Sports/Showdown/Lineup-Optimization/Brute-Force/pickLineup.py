4322efbbf01334ec6a5c31879a3a28ae0ec73793
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-12-12 17:45:00 -0500

    Update pickLineup.py

--- Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/pickLineup.py
+++ Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/pickLineup.py
@@ -2,7 +2,7 @@ import pandas as pd
 from itertools import combinations as comb
 
 def pickLineup(data, budget = 50000, num_utility = 5):
-    best_lineup = {"players": [], "total": 0, "salary": 0, "captain": None}
+    best_lineups = [{"players": [], "total": 0, "salary": 0, "captain": None}]
     data = data.sort_values(by=["points"],ascending=False).reset_index()
 
     for i in range(10):
@@ -11,19 +11,22 @@ def pickLineup(data, budget = 50000, num_utility = 5):
         c_points = data["capt. points"][i]
         remaining = budget - data["capt. salary"][i]
 
-        rems = data.drop([i]).reset_index()
+        rems = data.drop([i]).reset_index(drop=True)
+        print(f'For Capt {i}: {capt}, look: {rems.head(10)}')
         utility_lineups = list(comb(rems["player"], num_utility))
         
         for lineup in utility_lineups:
             lineup = list(lineup)
             lineup_data = rems[rems["player"].isin(lineup)]
             if c_salary + sum(lineup_data["salary"]) <= remaining:
-                if c_points + sum(lineup_data["points"]) > best_lineup["total"]:
+                if c_points + sum(lineup_data["points"]) > best_lineups[-1]["total"]:
                     lineup_total = c_points + sum(lineup_data["points"])
                     lineup_salary = c_salary + sum(lineup_data["salary"])
                     print("found a better line up")
-                    best_lineup = {"players": [capt]+list(lineup_data["player"]),
+                    best_lineup = best_lineup.append({"players": [capt]+list(lineup_data["player"]),
                                     "total": lineup_total, "salary": lineup_salary,
-                                    "captain": capt}
-            
-    return best_lineup
+                                    "captain": capt})
+    
+    best_lineups = sorted(best_lineups, key=lambda d: d['total']) 
+    
+    return best_lineups
