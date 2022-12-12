52a8bf04027fa58658f92677d1a43cf9e4ecef85
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-12-12 18:06:15 -0500

    Update run-optimizer.py

--- Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/run-optimizer.py
+++ Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/run-optimizer.py
@@ -6,13 +6,16 @@ print(f'directories: {listdir()}')
 
 d = pd.read_csv("Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/data/DKSalaries.csv")
 
-arine = pd.DataFrame({"player": d["Name"], "capt. salary": d["Salary"], "points": d["AvgPointsPerGame"]})
-arine["capt. points"] = [ p * 1.5 for p in arine["points"]]
-arine["salary"] = [ s / 1.5 for s in arine["capt. salary"]]
-print(f'first: {arine.head(10)}')
+arine = pd.DataFrame({"player": d["Name"], "salary": d["Salary"], "points": d["AvgPointsPerGame"]})
 out = ["Rondale Moore","Jakobi Meyers","Brian Hoyer", "Quinn Nordin","Bailey Zappe"]
 arine = arine[~arine["player"].isin(out)].reset_index(drop=True)
 arine_clean = arine[arine["salary"] > 2000]
-print(f'second: {arine.head(10)}')
 
-print(f'Chosen Lineup: {pickLineup(arine_clean,budget=50000,num_utility=5)}')
+players = list(arine["player"].unique())
+dset = pd.DataFrame({"player": [], "salary": [], "points": [], "capt. salary": [], "capt. points": []})
+for player in players:
+    d = arine[arine["player"] == player].sort_values(by = ["salary"],ascending = False).reset_index(drop=True)
+    dset = dset.append({"player": player, "points": d["points"][1], "salary": d["salary"][1],
+                        "capt. salary": d["salary"][0], "capt. points": d["points"][0]*1.5},ignore_index=True)
+
+print(f'Chosen Lineup: {pickLineup(dset,budget=50000,num_utility=5)}')
