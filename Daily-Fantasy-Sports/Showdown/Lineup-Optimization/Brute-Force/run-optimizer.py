5cd3a5456d30996ef113a27cdaa023b1c8724828
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-12-12 15:33:35 -0500

    Update run-optimizer.py

--- Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/run-optimizer.py
+++ Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/run-optimizer.py
@@ -11,4 +11,7 @@ arine["capt. points"] = [ p * 1.5 for p in arine["points"]]
 arine["capt. salary"] = [ s * 1.5 for s in arine["salary"]]
 arine_2000 = arine[arine["salary"] > 1900]
 
+out = ["Rondale Moore","Jakobi Meyers","Brian Hoyer", "Quinn Nordin"]
+arine = arine[~arine["player"].isin(out)].reset_index(drop=True)
+
 print(f'Chosen Lineup: {pickLineup(arine_2000,budget=50000,num_utility=5)}')
