892f17a968a8071d0f2ad8d413813b749ad9afdc
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-12-12 17:48:05 -0500

    Update run-optimizer.py

--- Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/run-optimizer.py
+++ Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/run-optimizer.py
@@ -9,9 +9,10 @@ d = pd.read_csv("Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/d
 arine = pd.DataFrame({"player": d["Name"], "capt. salary": d["Salary"], "points": d["AvgPointsPerGame"]})
 arine["capt. points"] = [ p * 1.5 for p in arine["points"]]
 arine["salary"] = [ s / 1.5 for s in arine["capt. salary"]]
-
+print(f'first: {arine.head(10)}')
 out = ["Rondale Moore","Jakobi Meyers","Brian Hoyer", "Quinn Nordin","Bailey Zappe"]
 arine = arine[~arine["player"].isin(out)].reset_index(drop=True)
 arine_clean = arine[arine["salary"] > 2000]
+print(f'second: {arine.head(10)}')
 
 print(f'Chosen Lineup: {pickLineup(arine_clean,budget=50000,num_utility=5)}')
