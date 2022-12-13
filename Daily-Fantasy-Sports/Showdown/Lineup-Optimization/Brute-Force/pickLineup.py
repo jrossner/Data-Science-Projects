82c75d699720435b3d3ef234d2691b1b427a4b93
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-12-12 19:26:21 -0500

    Update pickLineup.py

--- Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/pickLineup.py
+++ Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/pickLineup.py
@@ -14,22 +14,19 @@ def pickLineup(data, budget = 50000, num_utility = 5):
 
         rems = data.drop([i]).reset_index(drop=True)
         utility_lineups = list(comb(rems["player"], num_utility))
-        
+
         for lineup in utility_lineups:
             lineup = list(lineup)
             lineup_data = rems[rems["player"].isin(lineup)]
             if c_salary + sum(lineup_data["salary"]) <= remaining:
-                try:
-                    if c_points + sum(lineup_data["points"]) > best_lineups[-1]["total"]:
-                        lineup_total = c_points + sum(lineup_data["points"])
-                        lineup_salary = c_salary + sum(lineup_data["salary"])
-                        print("found a better line up")
-                        best_lineups.append({"players": [capt]+list(lineup_data["player"]),
-                                                "total": lineup_total, "salary": lineup_salary,
-                                                "captain": capt})
-                except:
-                    print(f'best: {best_lineups}')
-    
-    best_lineups = sorted(best_lineups, key=lambda d: d['total']) 
-    
+                if c_points + sum(lineup_data["points"]) > best_lineups[-1]["total"]:
+                    lineup_total = c_points + sum(lineup_data["points"])
+                    lineup_salary = c_salary + sum(lineup_data["salary"])
+                    print("found a better line up")
+                    best_lineups.append({"players": [capt]+list(lineup_data["player"]),
+                                            "total": lineup_total, "salary": lineup_salary,
+                                            "captain": capt})
+
+    best_lineups = sorted(best_lineups, key=lambda d: d['total'], reverse=True)
+
     return best_lineups
