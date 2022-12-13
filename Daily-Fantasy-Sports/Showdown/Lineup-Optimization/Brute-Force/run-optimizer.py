ad8afeb764505c87ff7e804d9647c866334e5db7
Jacob Rossner <66050953+jrossner@users.noreply.github.com>
2022-12-13 09:07:24 -0500

    Update run-optimizer.py

--- Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/run-optimizer.py
+++ Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/run-optimizer.py
@@ -2,10 +2,10 @@ from pickLineup import pickLineup
 import pandas as pd
 from os import listdir
 
-d = pd.read_csv("Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/data/DKSalaries.csv")
+d = pd.read_csv("Daily-Fantasy-Sports/Showdown/Lineup-Optimization/Brute-Force/data/phi/sac.csv")
 
 raw = pd.DataFrame({"player": d["Name"], "salary": d["Salary"], "points": d["AvgPointsPerGame"]})
-out = ["Rondale Moore","Jakobi Meyers","Brian Hoyer", "Quinn Nordin","Bailey Zappe","Colt McCoy"]
+out = ["Alex Len","Tyrese Maxey"]
 raw = raw[~raw["player"].isin(out)].reset_index(drop=True)
 
 players = list(raw["player"].unique())
@@ -14,6 +14,6 @@ for player in players:
     d = raw[raw["player"] == player].sort_values(by = ["salary"],ascending = False).reset_index(drop=True)
     dset.loc[len(dset)] = [player, d["points"][1], d["salary"][1], d["salary"][0], d["points"][0]*1.5]
 
-dset = dset[dset["salary"] > 2000]
+dset = dset[dset["salary"] > 100]
 
 print(f'Chosen Lineup: {pickLineup(dset,budget=50000,num_utility=5)}')
